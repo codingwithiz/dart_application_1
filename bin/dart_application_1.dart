c91550050d76f98c4ccc633643eebe58e57b64ea
Ing Zhen Lee <ingzhen2003@gmail.com>
2023-04-08 11:19:32 +0800

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,25 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+int a = 789;
+int ?b;
+late String description;
+int n = 89;
+
+void main(List<String> arguments) {
+  const name = "Ing Zhen";
+  var c = 1293;
+
+  const d2 = 60.0;
+  print(name);
+  print(c);
+  print(d2);
+  print(b);
+  
+
+  if(n>59){
+    print(true);
+  }else{
+    print(false);
+  }
+
+}
